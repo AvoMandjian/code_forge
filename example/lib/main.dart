361e75e3c7888a8510639c7c8aa2cdbe540dc9c4
Avo <avomandjian@gmail.com>
2026-01-28 18:28:08 +0400

    refactor: clean up main.dart by commenting out unused imports and code; update Xcode project files to include Pods support and streamline build configurations

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,10 @@
-import 'dart:io';
-import 'package:example/finder.dart';
-import 'package:path/path.dart' as p;
+// import 'dart:io';
+
 import 'package:code_forge/code_forge.dart';
+import 'package:example/finder.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+// import 'package:path/path.dart' as p;
 import 'package:re_highlight/languages/dart.dart';
 import 'package:re_highlight/styles/atom-one-dark-reasonable.dart';
 
@@ -20,19 +21,19 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final undoController = UndoRedoController();
-  final absFilePath = p.join(Directory.current.path, "lib/example_code.dart");
+  // final absFilePath = p.join(Directory.current.path, "lib/example_code.dart");
   CodeForgeController? codeController;
 
-  Future<LspConfig> getLsp() async {
-    final absWorkspacePath = p.join(Directory.current.path, "lib");
-    final data = await LspStdioConfig.start(
-      executable: "dart",
-      args: ["language-server", "--protocol=lsp"],
-      workspacePath: absWorkspacePath,
-      languageId: "dart",
-    );
-    return data;
-  }
+  // Future<LspConfig> getLsp() async {
+  //   final absWorkspacePath = p.join(Directory.current.path, "lib");
+  //   final data = await LspStdioConfig.start(
+  //     executable: "dart",
+  //     args: ["language-server", "--protocol=lsp"],
+  //     workspacePath: absWorkspacePath,
+  //     languageId: "dart",
+  //   );
+  //   return data;
+  // }
 
   @override
   void initState() {
@@ -53,42 +54,19 @@ class _MyAppState extends State<MyApp> {
           },
         ),
         body: SafeArea(
-          child: FutureBuilder<LspConfig>(
-            future: getLsp(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              }
-
-              if (!snapshot.hasData) {
-                return const Center(child: Text("Failed to load LSP"));
-              }
-
-              final lspConfig = snapshot.data!;
-              if (codeController == null ||
-                  codeController!.lspConfig != lspConfig) {
-                codeController = CodeForgeController(lspConfig: lspConfig);
-              }
-
-              return CodeForge(
-                undoController: undoController,
-                language: langDart,
-                editorTheme: atomOneDarkReasonableTheme,
-                controller: codeController,
-                textStyle: GoogleFonts.jetBrainsMono(),
-                filePath: absFilePath,
-                matchHighlightStyle: const MatchHighlightStyle(
-                  currentMatchStyle: TextStyle(
-                    backgroundColor: Color(0xFFFFA726),
-                  ),
-                  otherMatchStyle: TextStyle(
-                    backgroundColor: Color(0x55FFFF00),
-                  ),
-                ),
-                finderBuilder: (c, controller) =>
-                    FindPanelView(controller: controller),
-              );
-            },
+          child: CodeForge(
+            undoController: undoController,
+            language: langDart,
+            editorTheme: atomOneDarkReasonableTheme,
+            controller: codeController,
+            textStyle: GoogleFonts.jetBrainsMono(),
+            // filePath: absFilePath,
+            matchHighlightStyle: const MatchHighlightStyle(
+              currentMatchStyle: TextStyle(backgroundColor: Color(0xFFFFA726)),
+              otherMatchStyle: TextStyle(backgroundColor: Color(0x55FFFF00)),
+            ),
+            finderBuilder: (c, controller) =>
+                FindPanelView(controller: controller),
           ),
         ),
       ),
