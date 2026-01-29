bdd5c33d1858e0602b343acfb2a1321b29b9cf8d
Avo <avomandjian@gmail.com>
2026-01-29 17:58:28 +0400

    chore: Add universal_io dependency and update version to 6.0.0
    - Added universal_io package to pubspec.yaml for cross-platform I/O support.
    - Updated pubspec.lock to reflect the new dependency.
    - Modified iOS and macOS configuration files to include necessary Pod support files.
    - Cleaned up main.dart by commenting out unused code related to LSP configuration.

--- lib/LSP/lsp.dart
+++ lib/LSP/lsp.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:universal_io/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 part 'lsp_socket.dart';
